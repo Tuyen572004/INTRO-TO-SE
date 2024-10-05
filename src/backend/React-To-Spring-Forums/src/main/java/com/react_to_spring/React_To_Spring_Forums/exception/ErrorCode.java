c9384f84f771c4e0bf05f7223dcaf4ac4fb868d1
Zjn <129529039+zjn1404@users.noreply.github.com>
2024-10-05 23:05:15 +0700

    Add error code standards

--- src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/exception/ErrorCode.java
+++ src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/exception/ErrorCode.java
@@ -10,13 +10,14 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 //    Internal Server Error Undefined	9999
     UNCATEGORIZED_EXCEPTION(9999, HttpStatus.INTERNAL_SERVER_ERROR, "Uncategorized Error"),
-//    Internal Server Error	Developer	1xxx
+//    Internal Server Error	Developer error 1xxx
     INVALID_KEY(1001, HttpStatus.INTERNAL_SERVER_ERROR, "Invalid Key"),
-//    Bad Request	Client	2xxx
-//    Unauthorized	Client	3xxx (Unauthenticated error)
-//    Forbidden	Client	4xxx (Unauthorized error)
-    UNAUTHORIZED(3001, HttpStatus.FORBIDDEN, "Don't have permission"),
-//    Not Found	Client	5xxx
+//    Bad Request	Client Input Error	2xxx
+//    Existed Error 3xxx
+//    Not Found Error 4xxx
+//    Unauthorized	Client	5xxx (Unauthenticated error)
+//    Forbidden	Client	6xxx (Unauthorized error)
+    UNAUTHORIZED(6001, HttpStatus.FORBIDDEN, "Don't have permission"),
     ;
     final Integer code;
     final HttpStatus status;
