c35ff89f454e03cae73803da55ff14738eb9aa8b
Zjn <129529039+zjn1404@users.noreply.github.com>
2024-10-09 22:52:32 +0700

    Add expired token clean up server, check every 24h

--- src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/repository/InvalidatedTokenRepository.java
+++ src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/repository/InvalidatedTokenRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
     @Query("SELECT CASE WHEN COUNT(t) > 0 THEN TRUE ELSE FALSE END FROM invalidated_tokens t" +
             " WHERE t.accessId = :id OR t.refreshId = :id")
     boolean existsByAccessIdOrRefreshId(String id);
+
+    void deleteAllByExpirationTimeBefore(LocalDateTime time);
 }
