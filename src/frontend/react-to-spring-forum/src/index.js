dd65d04993368521f8a6c33fb567aae68cf812d0
Shung <shungisme@gmail.com>
2024-10-09 23:44:49 +0700

    set up redux - store user

--- src/frontend/react-to-spring-forum/src/index.js
+++ src/frontend/react-to-spring-forum/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
 
---
+++ src/frontend/react-to-spring-forum/src/store/index.js
@@ -0,0 +1,40 @@
+import { userReducer } from "./user/user-slice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+
+const rootReducer = combineReducers({
+	userSlice: userReducer,
+});
+
+const persistConfig = {
+	key: "root",
+	version: 1,
+	storage,
+	whitelist: ["authSlice"],
+};
+
+const persistedReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+	reducer: persistedReducers,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
