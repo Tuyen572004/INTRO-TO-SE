e6f1630a59acea7b4e8877c5fc96cd104b101b4d
Zjn <129529039+zjn1404@users.noreply.github.com>
2024-10-24 23:12:53 +0700

    Delete Role.java, check password in update user request

--- src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/enums/ReactName.java
+++ src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/enums/ReactName.java
@@ -1,8 +1,10 @@
 package com.react_to_spring.React_To_Spring_Forums.enums;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
+@Getter
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum ReactName {
     LIKE("LIKE"),
@@ -18,10 +20,6 @@ public enum ReactName {
 
     ReactName(String name) {
         this.name = name;
-    }
-
-    public String getName() {
-        return name;
     }
 
 }
