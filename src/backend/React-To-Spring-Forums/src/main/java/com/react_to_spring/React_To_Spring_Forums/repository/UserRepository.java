df4e7faad08e8f5a4404a10b6ef1fe93017af3f5
vuthevy1209 <vuthevy12092004@gmail.com>
2024-12-09 18:38:57 +0700

    fixbug: getMyAccount Api

--- src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/repository/UserRepository.java
+++ src/backend/React-To-Spring-Forums/src/main/java/com/react_to_spring/React_To_Spring_Forums/repository/UserRepository.java
@@ -14,5 +14,7 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByUsername(String username);
 
+    Optional<User> findById(String id);
+
     Optional<User> findByEmail(String email);
 }
